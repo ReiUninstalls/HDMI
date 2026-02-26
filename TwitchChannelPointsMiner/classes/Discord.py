7af02e69d2f546486018b4346dd5506da02bd836
ReiUninstalls <reireinstalls@gmail.com>
2026-02-25 22:08:10 -0800

    Update Discord.py

--- TwitchChannelPointsMiner/classes/Discord.py
+++ TwitchChannelPointsMiner/classes/Discord.py
@@ -18,7 +18,7 @@ class Discord(object):
                 url=self.webhook_api,
                 data={
                     "content": dedent(message),
-                    "username": "Kurumi Pussy",
+                    "username": "Kurumi Underwear",
                     "avatar_url": "https://media.discordapp.net/attachments/1476107573387202642/1476460252085096500/71c02e26982abb2c82503f0f18a56ea59ab85d12122f4595b7ae9b806a6c29b3.png?ex=69a13443&is=699fe2c3&hm=40320fc1853f571b32e7191b600e064786b1d5e0a86dad6d78f73fcb2c10491b&=&format=webp&quality=lossless",
                 },
             )
